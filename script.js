ba4b37f2cf6ea225de2691979d22b0bffd3a4473
Arhivater <54925901+Arhivater@users.noreply.github.com>
2019-10-24 11:23:42 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+function test(quest, value) {
+    if (quest == 4 || quest == 5 || quest == 6){
+        if (array[quest] == undefined){
+            array[quest] = String(value);
+            console.log(value);
+        }
+        else{
+            array[quest] += String(value);
+            console.log(value);
+        }
+    }
+    else{
+        array[quest] = String(value);
+        console.log(value);
+    }
+};
+
+function checkArray(){
+    for (let i = 0; i < array.length; i++){
+        console.log((i+1) + ") " + array[i]);
+    }
+};
+
+function checkTrue(){
+    let t = 0;
+    for (let i = 0; i < array.length; i++) {
+        if ((i == 4 || i == 5 || i == 6) && (array[i] != undefined)){
+            let arrayMore = array[i].split('');
+            let arrayMoreTrue = trueArray[i].split('');
+            let x = 0;
+            for (let i = 0; i < arrayMore.length; i++) {
+                for (let j = 0; j < arrayMoreTrue.length; j++) {
+                    if (arrayMore[i] == arrayMoreTrue[j]){
+                        x++;
+                        break;
+                    }
+                }
+            }
+            if (x == trueArray[i].length) {
+                t++;
+            }
+        }
+        else {
+            if (array[i] == trueArray[i]){
+                t++;
+            }
+        }
+
+    }
+    document.getElementById('place-for-result').innerHTML='Количество правильных ответов: ' + t;
+};
+
+function resetAnswer(quest) {
+    array[quest] = undefined;
+};
+
+let array = new Array(10);
+let trueArray = ['1', '3', '2', '2', '1234', '24', '4', 'a', 'html', 'ol'];
