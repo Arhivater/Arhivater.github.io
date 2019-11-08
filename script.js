3336a8b7bcce32e72efb1222106aa2396b557e3e
Arhivater <54925901+Arhivater@users.noreply.github.com>
2019-11-09 00:50:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -57,8 +57,7 @@ let array = new Array(10);
 let trueArray = ['1', '4', '2', '3', '123', '14', '15', '6', 'Иллидану', 'Анубарак'];
 
 function disable() {
-document.getElementById("check").disabled = true;
+document.getElementById("proverka").disabled = true;
 }
 function enable() {
-document.getElementById("check").disabled = false;
-}
+document.getElementById("proverka").disabled = false;
