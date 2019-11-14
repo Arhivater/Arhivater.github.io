22f901b584e10ce09def5f34cd8c4e4e7bd82da8
Arhivater <54925901+Arhivater@users.noreply.github.com>
2019-11-14 10:26:48 +0700

    Create ScriptLab№5.js

---
+++ ScriptLab№5.js
@@ -0,0 +1,46 @@
+function loadTable(){
+    let myTable = document.getElementById("iTable");
+    let fild_size = document.getElementById("filed_size").value;
+    let nx = fild_size;
+    let ny = fild_size;
+    myTable.innerHTML = "";
+    for (let i=0; i < nx; ++i){
+        let newTableLine = document.createElement("tr");
+        for (let j=0; j < ny; ++j) {
+            let newTableCell = document.createElement("td");
+
+            newTableCell.setAttribute("id","cell_"+(i+1)+"_"+(j+1));
+
+            //newTableCell.setAttribute("cellStatus","nothing");
+            newTableCell.cellStatus = "nothing";
+
+            newTableCell.addEventListener("mousedown",cellMouseDown);
+            newTableLine.appendChild(newTableCell);
+        }
+        myTable.appendChild(newTableLine);
+    }
+}
+
+function cellMouseDown(e) {
+    e = e || window.event;
+    //alert(e.which);
+    switch (e.which) {
+        case 1:
+            if (this.style.backgroundImage == "") {
+            this.style.backgroundImage = "url(crug.png)";
+            this.innerHTML = "O";
+            //this.innerHTML = this.;cellStatus
+            checkWin (this.id, "crug");
+        }
+            break;
+        case 3:
+            if (this.style.backgroundImage == "") {
+                this.style.backgroundImage = "url(crest.png)";
+                this.innerHTML = "X";
+                checkWin(this.id, "crest");
+            }
+            break;
+    }
+}
+
+document.oncontextmenu = function () {return false;};
