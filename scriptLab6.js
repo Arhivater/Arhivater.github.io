31f92fdc7250603f6539be5bb9517aaa583d2aa0
Arhivater <54925901+Arhivater@users.noreply.github.com>
2019-11-28 01:23:23 +0700

    Update scriptLab6.js

--- scriptLab6.js
+++ scriptLab6.js
@@ -31,17 +31,51 @@ function cellMouseDown(e) {
             this.style.backgroundImage = "url(crug.png)";
             this.innerHTML = "O";
             //this.innerHTML = this.;cellStatus
-            checkWin (this.id, "crug");
+            VictoryCheck(this.id, "crug");
         }
             break;
         case 3:
             if (this.style.backgroundImage == "") {
                 this.style.backgroundImage = "url(crest.png)";
                 this.innerHTML = "X";
-                checkWin(this.id, "crest");
+                VictoryCheck(this.id, "crest");
             }
             break;
     }
+}
+
+function CheckDirection(row, cell, typeField, directionRow, directionCell)
+{
+    let currentRow = row + directionRow;
+    let currentCell = cell + directionCell;
+    let id = "cell_" + (currentRow + 1) + "_" + (currentCell + 1);
+    let myCell = document.getelementByid(id);
+    if (myCell !- null && typeField == myCell.cellStatus)
+    {
+        return 1 + CheckDirection(currentRow, currentCell, typeField, directionRow, directionCell);
+    }
+    return 0;
+}
+
+Function VictoryCheck (row, cell, typefield)
+{
+    let VictoryСondition = 5;
+    let HorizontalCheck =  CheckDirection(row,cell,typeField, 0, -1) + CheckDirection(row,cell,typeField, 0, 1) + 1;
+    let VerticalСheck = CheckDirection(row,cell,typeField, -1, 0) + CheckDirection(row,cell,typeField, 1, 0) + 1;
+    let DecreasegDiagonalСheck =  CheckDirection(row,cell,typeField, -1, -1) + CheckDirection(row,cell,typeField, 1, 1) + 1;
+    let IncreasingDiagonalСheck  = CheckDirection(row,cell,typeField, -1, 1) + CheckDirection(row,cell,typeField, 1, -1) + 1;
+    
+    if(HorizontalCheck >= VictoryСondition || VerticalСheck >= VictoryСondition || DecreasegDiagonalСheck >= VictoryСondition || IncreasingDiagonalСheck >= VictoryСondition)
+    {
+        if (typeFiled == "crest")
+        {   
+            document.getElementById("Cross").innerHTML = "X Victory";
+        } else if (typefield == "crug")
+          {
+            document.getElementById("Cross").innerHTML = "O Victory";
+          }
+        document.getElementById("MyTable").style.display = "none";
+    }    
 }
 
 document.oncontextmenu = function () {return false;};
