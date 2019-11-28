5693185632e2079c55a295eb44d284e9708b434d
Arhivater <54925901+Arhivater@users.noreply.github.com>
2019-11-28 20:47:33 +0700

    Update scriptLab6.js

--- scriptLab6.js
+++ scriptLab6.js
@@ -1,4 +1,5 @@
 let VictoryСondition = 5;
+let turn = 0;
 function loadTable(){
     let myTable = document.getElementById("iTable");
     let fild_width = document.getElementById("filed_width").value;
@@ -28,19 +29,30 @@ function cellMouseDown(e) {
     let index1 = el.closest("tr").rowIndex;
     let index2 = el.closest("td").cellIndex;
     //alert(e.which);
+    
     switch (e.which) {
         case 1:
             if (this.style.backgroundImage == "") {
             //this.style.backgroundImage = "url(crug.png)";
-            this.cellStatus =  = "O";
+            this.cellStatus =   "O";
             //this.innerHTML = this.;cellStatus
             VictoryCheck(index1, index2, this.cellStatus);
         }
             break;
+        case 2:
+            if (turn == 2){
+                this.cellStatus =   "O";
+                --turn;
+            } else {
+                this.cellStatus =   "X";
+                ++turn; 
+            }
+            VictoryCheck(index1, index2, this.cellStatus);
+            break;
         case 3:
             if (this.style.backgroundImage == "") {
                 //this.style.backgroundImage = "url(crest.png)";
-                this.cellStatus = = "X";
+                this.cellStatus =  "X";
                 VictoryCheck(index1, index2, this.cellStatus);
             }
             break;
@@ -53,32 +65,27 @@ function CheckDirection(row, cell, typeField, directionRow, directionCell)
     let currentCell = cell + directionCell;
     let id = "cell_" + (currentRow + 1) + "_" + (currentCell + 1);
     let myCell = document.getelementByid(id);
-    if (myCell !- null && typeField == myCell.cellStatus)
+    if (myCell != null && typeField == myCell.cellStatus)
     {
         return 1 + CheckDirection(currentRow, currentCell, typeField, directionRow, directionCell);
     }
     return 0;
 }
 
-Function VictoryCheck (row, cell, typefield)
-{
-    
-    let HorizontalCheck =  CheckDirection(row,cell,typeField, 0, -1) + CheckDirection(row,cell,typeField, 0, 1) + 1;
-    let VerticalСheck = CheckDirection(row,cell,typeField, -1, 0) + CheckDirection(row,cell,typeField, 1, 0) + 1;
-    let DecreasegDiagonalСheck =  CheckDirection(row,cell,typeField, -1, -1) + CheckDirection(row,cell,typeField, 1, 1) + 1;
-    let IncreasingDiagonalСheck  = CheckDirection(row,cell,typeField, -1, 1) + CheckDirection(row,cell,typeField, 1, -1) + 1;
-    
-    if(HorizontalCheck >= VictoryСondition || VerticalСheck >= VictoryСondition || DecreasegDiagonalСheck >= VictoryСondition || IncreasingDiagonalСheck >= VictoryСondition)
-    {
-        if (typeFiled == "crest")
-        {   
-           // document.getElementById("Cross").innerHTML = "X Victory";
-        } else if (typefield == "crug")
-          {
-           // document.getElementById("Cross").innerHTML = "O Victory";
-          }
-      //  document.getElementById("MyTable").style.display = "none";
-    }    
+function VictoryCheck(row, cell, typeField) {
+    let HorizontalCheck = CheckDirection(row, cell, typeField, 0, -1) + CheckDirection(row, cell, typeField, 0, 1) + 1;
+    let VerticalСheck = CheckDirection(row, cell, typeField, -1, 0) + CheckDirection(row, cell, typeField, 1, 0) + 1;
+    let DecreasegDiagonalСheck = CheckDirection(row, cell, typeField, -1, -1) + CheckDirection(row, cell, typeField, 1, 1) + 1;
+    let IncreasingDiagonalСheck = CheckDirection(row, cell, typeField, -1, 1) + CheckDirection(row, cell, typeField, 1, -1) + 1;
+     if (HorizontalCheck => VictoryСondition || VerticalСheck => VictoryСondition || DecreasegDiagonalСheck => VictoryСondition || IncreasingDiagonalСheck => VictoryСondition) {
+        if (typeField == "crest")
+           // document.getElementById("Cross").innerHTML = "Победил Альянс";
+        else if (typeField == "crug")
+           // document.getElementById("Cross").innerHTML = "Победила Орда";
+       // document.getElementById("Table1").style.display = "none";
+    }
+    else if (CheckDrow())
+       // document.getElementById("Cross").innerHTML = "Ничья";
 }
 
 document.oncontextmenu = function () {return false;};
