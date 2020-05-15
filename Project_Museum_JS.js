3d7aba808d6d9118a05880e8ff10ebdcf6e6855e
GeorgeVorob <54977627+GeorgeVorob@users.noreply.github.com>
2020-05-15 16:49:45 +0700

    reindent

--- Project_Museum_JS.js
+++ Project_Museum_JS.js
@@ -1,21 +1,28 @@
 let Paintings_img = ["https://upload.wikimedia.org/wikipedia/commons/8/83/Leonardo_da_Vinci_-_Portrait_of_a_Musician.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/800px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg","https://sun9-58.userapi.com/c845416/v845416689/826f4/8R0ZQXbkNXQ.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/757px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/The_Scream.jpg/470px-The_Scream.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/God2-Sistine_Chapel.png/800px-God2-Sistine_Chapel.png","https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Shishkin%2C_Ivan_-_Morning_in_a_Pine_Forest.jpg/800px-Shishkin%2C_Ivan_-_Morning_in_a_Pine_Forest.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Hovhannes_Aivazovsky_-_The_Ninth_Wave_-_Google_Art_Project.jpg/800px-Hovhannes_Aivazovsky_-_The_Ninth_Wave_-_Google_Art_Project.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Karl_Brullov_-_The_Last_Day_of_Pompeii_-_Google_Art_Project.jpg/800px-Karl_Brullov_-_The_Last_Day_of_Pompeii_-_Google_Art_Project.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Apotheosis.jpg/800px-Apotheosis.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/John_William_Waterhouse_-_Fair_Rosamund.jpg/444px-John_William_Waterhouse_-_Fair_Rosamund.jpg","http://ayvazovskiy.su/wp-content/uploads/2017/02/ledgor.jpg"];					 
-				 
+
 let Authors_img = ["https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Leonardo_self.jpg/150px-Leonardo_self.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Leonardo_self.jpg/150px-Leonardo_self.jpg","https://upload.wikimedia.org/wikipedia/commons/1/1f/Thomas_Cole.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/VincentVanGoghFoto.jpg/150px-VincentVanGoghFoto.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Edvard_Munch_1912.jpg/200px-Edvard_Munch_1912.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Miguel_%C3%81ngel%2C_por_Daniele_da_Volterra_%28detalle%29.jpg/518px-Miguel_%C3%81ngel%2C_por_Daniele_da_Volterra_%28detalle%29.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Ivan_Shishkin_restoration.jpg/250px-Ivan_Shishkin_restoration.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Aivazovsky_-_Self-portrait_1874.jpg/546px-Aivazovsky_-_Self-portrait_1874.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Brjullov.jpg/512px-Brjullov.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Vasili_Vereshchagin.jpg/432px-Vasili_Vereshchagin.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/John_William_Waterhouse_001.jpg/376px-John_William_Waterhouse_001.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Brjullov.jpg/512px-Brjullov.jpg"];
-				   
+
 let Paintings_name = ["«Портрет музыканта»","«Портре́т госпожи́ Ли́зы дель Джоко́ндо»","«Крушение»","«Звёздная Ночь»","«Крик»","«Сотворение Адама»","«Утро в сосновом лесу»","«Девятый вал»","«После́дний день Помпе́и»","«Апофеоз войны»","«Прекрасная Розамунда»","«Ледяные горы»"];
-				   
+
 let Paintig_date = ["1490-2","варьируется между 1503 и 1505 годами","1836 год","1889 год","в промежутке между 1893 и 1910 годами","около 1511 года","1889 год","1850 год","1833 год","1871 год","около 1150 года","1870 год"];
-					
+
 let Author_name = ["Леона́рдо ди сер Пье́ро да Ви́нчи","Леона́рдо ди сер Пье́ро да Ви́нчи","Томас Коул","Винсент ван Гог","Эдвард Мунк","Микела́нджело Буонарро́ти","И. И. Шишкин, К. А. Савицкий","Айвазовский Иван Константинович","Брюллов, Карл Павлович","Василий Верещагин","Джон Уильям Уотерхаус","Айвазовский Иван Константинович"];
 
 let Paintig_description = ["Авторство Леонардо да Винчи оспаривается редко, хотя и существует предположение, что автором портрета является не Леонардо, а Антонелло да Мессина.","Одно из самых известных произведений живописи. Считается, что на картине изображена Лиза Герардини, супруга флорентийского торговца шёлком Франческо дель Джокондо.","Одно из картин входящих в серию из 5 картин Томаса Коула под названием <<Путь Империи>>.","Представляет вид из восточного окна спальни Ван Гога в Сен-Реми-де-Прованс на предрассветное небо и вымышленную деревню.","Cерия картин норвежского художника-экспрессиониста Эдварда Мунка. На них изображена кричащая в отчаянии человеческая фигура на фоне кроваво-красного неба и крайне обобщённого пейзажного фона.","Фреска Микеланджело, является четвёртой из девяти центральных композиций потолка Сикстинской капеллы, посвящённых девяти сюжетам книги Бытия. Фреска иллюстрирует эпизод: 'И сотворил Бог человека по образу Своему'","Савицкий написал медведей. но коллекционер П. М. Третьяков стёр его подпись, так что автором картины часто указывается один Шишкин.Картина пользуется популярностью благодаря композиционному включению в пейзажное полотно элементов анималистической сюжетности. Работа детально передает состояние природы, увиденное художником на острове Городомля. Показан не глухой дремучий лес, а солнечный свет, пробивающийся сквозь колонны высоких деревьев. Чувствуют приближение утра резвящиеся медвежата.","Живописец изображает море после очень сильного ночного шторма и людей, потерпевших кораблекрушение. Лучи солнца освещают громадные волны. Самая большая из них — девятый вал — готова обрушиться на людей, пытающихся спастись на обломках мачты.","На картине изображены события в Помпеях во время катастрофического извержения Везувия, которое произошло в 79 году нашей эры.","На раме сделана надпись: «Посвящается всем великим завоевателям — прошедшим, настоящим и будущим». Изначально полотно называлось «Торжество Тамерлана». Замысел был связан с Тамерланом, войска которого оставляли за собой такие пирамиды черепов, однако картина не носит конкретно-исторический характер.","На картине изображена дочь Уолтера Фиц-Ричарда де Клиффорд и Маргариты де Тосни, любовница короля Англии Генриха II Плантагенета. За свою красоту получила прозвища «Прекрасная Розамунда»","Полотно Айвазовского Ледяные горы является известным на весь мир, автор написал, чтобы увековечить великое открытие отечественных мореходов, нашедших континент на самом юге Земли."];
-						   						   
+
 //////////////////////////////////////////////////////////////
 
-var Main_section = document.getElementById('Main_section');						
+var Main_section = document.getElementById('Main_section');	
+//создане секции
 for(let i = 1; i <= 4 ; i++) {
-	Main_section.insertAdjacentHTML("beforeend", "<div id='Paintings_section"+'_'+i+"' class='w3-row-padding w3-section'>");		
+	var section = document.createElement("div");
+	section.classList.add("w3-row-padding");
+	section.classList.add("w3-section");
+	section.id=("Paintings_section_"+i);
+	Main_section.appendChild(section);
+	//Main_section.insertAdjacentHTML("beforeend", "<div id='Paintings_section"+'_'+i+"' class='w3-row-padding w3-section'>");		
 }
+//создание места для кратины
 for(let j = 1; j <= 4 ; j++) {
 	var P_section = document.getElementById("Paintings_section"+'_'+j);
 	for(let i = 1; i <= 3 ; i++) {
@@ -23,6 +30,7 @@ for(let j = 1; j <= 4 ; j++) {
 	}
 }
 var temp = 0;
+//добавляет секцию в раннее осзданное место
 for(let j = 1; j <= 4 ; j++) {
 	for(let i = 1; i <= 3 ; i++) {
 		var Painting_img = document.getElementById("Painting"+'_'+j+'_'+i);
@@ -32,19 +40,19 @@ for(let j = 1; j <= 4 ; j++) {
 }
 
 for(let j = 1; j <= 4 ; j++) {
-	for(let i = 1; i <= 3 ; i++) {
-		var Painting_content = document.getElementById("Painting"+'_'+j+'_'+i);
-		Painting_content.insertAdjacentHTML("beforeend", "<div id='Painting_content"+'_'+j+'_'+i+"' class='w3-container w3-white'>");
-		var Content = document.getElementById("Painting_content"+'_'+j+'_'+i);
-		Content.insertAdjacentHTML("beforeend", "<div id='Content_media"+'_'+j+'_'+i+"' class='media'>");
-	}
+for(let i = 1; i <= 3 ; i++) {
+var Painting_content = document.getElementById("Painting"+'_'+j+'_'+i);
+Painting_content.insertAdjacentHTML("beforeend", "<div id='Painting_content"+'_'+j+'_'+i+"' class='w3-container w3-white'>");
+var Content = document.getElementById("Painting_content"+'_'+j+'_'+i);
+Content.insertAdjacentHTML("beforeend", "<div id='Content_media"+'_'+j+'_'+i+"' class='media'>");
+}
 }
 
 for(let j = 1; j <= 4 ; j++) {
-	for(let i = 1; i <= 3 ; i++) {
-		var Media = document.getElementById("Content_media"+'_'+j+'_'+i);
-		Media.insertAdjacentHTML("beforebegin", "<div class='media-left' style='padding-top: 35px;'>");
-	}
+for(let i = 1; i <= 3 ; i++) {
+var Media = document.getElementById("Content_media"+'_'+j+'_'+i);
+Media.insertAdjacentHTML("beforebegin", "<div class='media-left' style='padding-top: 35px;'>");
+}
 }
 
 // P_section.insertAdjacentHTML("beforebegin", "<div class='media-left' style='padding-top: 35px;'>");
@@ -56,80 +64,80 @@ for(let j = 1; j <= 4 ; j++) {
 /*
 let i = 4; // i = 4 - это кол-во секций для картин. кол-во секций = кол-во картин / 3
 while (i) {
-	P_section.insertAdjacentHTML("beforebegin", "<div class='w3-row-padding w3-section'>");  
-		let j = 3; // const x3
-		while (j) { // когда i будет равно 0, условие станет ложным, и цикл остановится
-			P_section.insertAdjacentHTML("beforebegin", "<div class='w3-third w3-container w3-margin-bottom'>");
-			
-				P_section.insertAdjacentHTML("beforebegin", "<img class='img_Paintings' src='"+ Paintings_img[1] +"' alt='Painting_Image'>");
-				
-				
-				
-				
-				P_section.insertAdjacentHTML("beforebegin", "<div class='w3-container w3-white'>");
-				
-				
-				
-					
-					P_section.insertAdjacentHTML("beforebegin", "<div class='media'>");
-					
-					
-						
-						P_section.insertAdjacentHTML("beforebegin", "<div class='media-left' style='padding-top: 35px;'>");
-							P_section.insertAdjacentHTML("beforebegin", "<img class='Author' src='"+ Authors_img[1] +"' alt='Author_Image'>");
-						P_section.insertAdjacentHTML("beforebegin", "</div>");
-						
-						P_section.insertAdjacentHTML("beforebegin", "<div class='media-body' style='padding-top: 5px;'>");
-							P_section.insertAdjacentHTML("beforebegin", "<h4 class='media-heading'>"+ Paintings_name[1] +"</h4>");
-							P_section.insertAdjacentHTML("beforebegin", "<h4><small><i>Дата написания:"+ Paintig_date[1] +"</i></small></h4>");
-							P_section.insertAdjacentHTML("beforebegin", "<p>Автор: "+ Author_name[1] +"</p>");
-							P_section.insertAdjacentHTML("beforebegin", "<p>"+ Paintig_description[1] +"</p>");
-						
-						P_section.insertAdjacentHTML("beforebegin", "</div>");
-						
-						
-						
-						
-					P_section.insertAdjacentHTML("beforebegin", "</div>");
-					
-					
-					
-				
-				P_section.insertAdjacentHTML("beforebegin", "</div>");
-				
-				
-				
-				
-				
-				
-			P_section.insertAdjacentHTML("beforebegin", "</div>");
-			j--;
-		}
-	P_section.insertAdjacentHTML("beforebegin", "</div>");
-	i--;
+P_section.insertAdjacentHTML("beforebegin", "<div class='w3-row-padding w3-section'>");  
+let j = 3; // const x3
+while (j) { // когда i будет равно 0, условие станет ложным, и цикл остановится
+P_section.insertAdjacentHTML("beforebegin", "<div class='w3-third w3-container w3-margin-bottom'>");
+
+P_section.insertAdjacentHTML("beforebegin", "<img class='img_Paintings' src='"+ Paintings_img[1] +"' alt='Painting_Image'>");
+
+
+
+
+P_section.insertAdjacentHTML("beforebegin", "<div class='w3-container w3-white'>");
+
+
+
+
+P_section.insertAdjacentHTML("beforebegin", "<div class='media'>");
+
+
+
+P_section.insertAdjacentHTML("beforebegin", "<div class='media-left' style='padding-top: 35px;'>");
+P_section.insertAdjacentHTML("beforebegin", "<img class='Author' src='"+ Authors_img[1] +"' alt='Author_Image'>");
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+
+P_section.insertAdjacentHTML("beforebegin", "<div class='media-body' style='padding-top: 5px;'>");
+P_section.insertAdjacentHTML("beforebegin", "<h4 class='media-heading'>"+ Paintings_name[1] +"</h4>");
+P_section.insertAdjacentHTML("beforebegin", "<h4><small><i>Дата написания:"+ Paintig_date[1] +"</i></small></h4>");
+P_section.insertAdjacentHTML("beforebegin", "<p>Автор: "+ Author_name[1] +"</p>");
+P_section.insertAdjacentHTML("beforebegin", "<p>"+ Paintig_description[1] +"</p>");
+
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+
+
+
+
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+
+
+
+
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+
+
+
+
+
+
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+j--;
+}
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+i--;
 }
 */
 
 /*
-	P_section.insertAdjacentHTML("beforebegin", "<div id='Paintings_section' class='w3-row-padding w3-section'>");  
-		P_section.insertAdjacentHTML("beforebegin", "<div class='w3-third w3-container w3-margin-bottom'>");
-			P_section.insertAdjacentHTML("beforebegin", "<img class='img_Paintings' src='"+ Paintings_img[1] +"' alt='Painting_Image'>");
-			P_section.insertAdjacentHTML("beforebegin", "<div class='w3-container w3-white'>");
-				P_section.insertAdjacentHTML("beforebegin", "<div class='media'>");
-					P_section.insertAdjacentHTML("beforebegin", "<div class='media-left' style='padding-top: 35px;'>");
-						P_section.insertAdjacentHTML("beforebegin", "<img class='Author' src='"+ Authors_img[1] +"' alt='Author_Image'>");
-					P_section.insertAdjacentHTML("beforebegin", "</div>");
-					P_section.insertAdjacentHTML("beforebegin", "<div class='media-body' style='padding-top: 5px;'>");
-						P_section.insertAdjacentHTML("beforebegin", "<h4 class='media-heading'>"+ Paintings_name[1] +"</h4>");
-						P_section.insertAdjacentHTML("beforebegin", "<h4><small><i>Дата написания:"+ Paintig_date[1] +"</i></small></h4>");
-						P_section.insertAdjacentHTML("beforebegin", "<p>Автор: "+ Author_name[1] +"</p>");
-						P_section.insertAdjacentHTML("beforebegin", "<p>"+ Paintig_description[1] +"</p>");
-					P_section.insertAdjacentHTML("beforebegin", "</div>");
-				P_section.insertAdjacentHTML("beforebegin", "</div>");
-			P_section.insertAdjacentHTML("beforebegin", "</div>");
-		P_section.insertAdjacentHTML("beforebegin", "</div>");
-	P_section.insertAdjacentHTML("beforebegin", "</div>");
-	
+P_section.insertAdjacentHTML("beforebegin", "<div id='Paintings_section' class='w3-row-padding w3-section'>");  
+P_section.insertAdjacentHTML("beforebegin", "<div class='w3-third w3-container w3-margin-bottom'>");
+P_section.insertAdjacentHTML("beforebegin", "<img class='img_Paintings' src='"+ Paintings_img[1] +"' alt='Painting_Image'>");
+P_section.insertAdjacentHTML("beforebegin", "<div class='w3-container w3-white'>");
+P_section.insertAdjacentHTML("beforebegin", "<div class='media'>");
+P_section.insertAdjacentHTML("beforebegin", "<div class='media-left' style='padding-top: 35px;'>");
+P_section.insertAdjacentHTML("beforebegin", "<img class='Author' src='"+ Authors_img[1] +"' alt='Author_Image'>");
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+P_section.insertAdjacentHTML("beforebegin", "<div class='media-body' style='padding-top: 5px;'>");
+P_section.insertAdjacentHTML("beforebegin", "<h4 class='media-heading'>"+ Paintings_name[1] +"</h4>");
+P_section.insertAdjacentHTML("beforebegin", "<h4><small><i>Дата написания:"+ Paintig_date[1] +"</i></small></h4>");
+P_section.insertAdjacentHTML("beforebegin", "<p>Автор: "+ Author_name[1] +"</p>");
+P_section.insertAdjacentHTML("beforebegin", "<p>"+ Paintig_description[1] +"</p>");
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+P_section.insertAdjacentHTML("beforebegin", "</div>");
+
 
 
 */
