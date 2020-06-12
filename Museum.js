dae025f92c963301c884b21548dd966afd249ff7
Arhivater <54925901+Arhivater@users.noreply.github.com>
2020-06-12 16:25:52 +0700

    11

--- Museum.js
+++ Museum.js
@@ -18,7 +18,7 @@ xhr.onload = function(e) {
         }
         xhr.send();
     }
-var test = readJSON("./test.json");
+var test = readJSON("./List.json");
 console.log(test);
 var parsed = JSON.parse(kappa.substring(0, kappa.length - 1));
 console.log(parsed.Paintings[1].Paintings_name);
